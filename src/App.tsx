ac15f2723d9141942ceabbf65df17bf2fca797a1
Maksim Lebedev <monk527@yandex.ru>
2024-07-20 02:21:32 +0500

    add components: Accordion, Rating, PageTitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { log } from "console";
+import { PageTitle } from "./components/appTitle/PageTitle";
+import { Accordion } from "./components/accordion/Accordion";
+import { Rating } from "./components/rating/Rating";
 
 function App() {
   console.log("App rendering");
   return (
     <div className="App">
-      <AppTitle />
-      <Rating />
-      <Accordion />
-      <Rating />
+      <PageTitle title="This is APP component" />
+      <PageTitle title="My friends" />
+      Article 1
+      <Rating value={3} />
+      <Accordion title="Меню" />
+      <Accordion title="МИНЮ" />
+      Article 2
+      <Rating value={0} />
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
     </div>
-  );
-}
-
-function AppTitle() {
-  console.log("AppTitle rendering");
-  return <>This is APP component</>;
-}
-
-function Rating() {
-  console.log("Rating rendering");
-  return (
-    <div>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </div>
-  );
-}
-
-function Star() {
-  console.log("Star rendering");
-
-  return <div>star</div>;
-}
-
-function Accordion() {
-  console.log("Accordion rendering");
-  return (
-    <div>
-      <AccordionTitle />
-      <AccordionBody />
-    </div>
-  );
-}
-
-function AccordionTitle() {
-  console.log("AccordionTitle rendering");
-
-  return <h3>Меню</h3>;
-}
-
-function AccordionBody() {
-  console.log("AccordionBody rendering");
-
-  return (
-    <>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </>
   );
 }
 
---
+++ src/components/appTitle/PageTitle.tsx
@@ -0,0 +1,4 @@
+export function PageTitle(props: any) {
+  console.log("PageTitle rendering");
+  return <h1>{props.title}</h1>;
+}
