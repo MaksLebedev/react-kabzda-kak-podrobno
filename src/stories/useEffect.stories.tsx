32415205283be993ddfa38aeec8f569ddb669454
Maksim Lebedev <monk527@yandex.ru>
2024-11-19 16:07:35 +0500

    22 - useEffect, setTimeout, setInterval

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -9,19 +9,19 @@ export const SimpleExample = () => {
   const [counter, setCounter] = useState(1);
   console.log("SimpleExample");
 
-// useEffect срабатывает, когда уже произошла отрисовка
+  // useEffect срабатывает, когда уже произошла отрисовка
 
-//   useEffect(() => {
-//     console.log("useEffect");
-//     // sideeffect:
-//     // api.getUsers().then('') - запрос на сервер
-//     // setInteval - асинхронная функция
-//     // indexedDB - хранилище данных
-//     // documment.getElementById - поиск элемента
-//     // document.title = 'User '
-//   });
+  //   useEffect(() => {
+  //     console.log("useEffect");
+  //     // sideeffect:
+  //     // api.getUsers().then('') - запрос на сервер
+  //     // setInteval - асинхронная функция
+  //     // indexedDB - хранилище данных
+  //     // documment.getElementById - поиск элемента
+  //     // document.title = 'User '
+  //   });
 
-useEffect(() => {
+  useEffect(() => {
     console.log("useEffect every render");
     document.title = counter.toString();
   });
@@ -36,14 +36,44 @@ useEffect(() => {
     document.title = counter.toString();
   }, [counter]);
 
-
-
   return (
     <>
       Hello, {counter}
       {fake}
       <button onClick={() => setFake(fake + 1)}>fake+</button>
       <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+  );
+};
+
+export const SetIntervalExample = () => {
+  const [fake, setFake] = useState(1);
+  const [counter, setCounter] = useState(1);
+  console.log("SetIntervalExample");
+
+  // асинхронные функции запускаем внутри useEffect
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     console.log("tick:" + counter);
+  //     setCounter((state) => state + 1);
+  //   }, 1000);
+  // }, []);
+
+  
+  const currentDate = new Date();
+  console.log(currentDate.getHours());
+   
+
+  return (
+    <>
+      Hello, counter: {counter} - fake: {fake}
+      {/* <button onClick={() => setFake(fake + 1)}>fake+</button>
+      <button onClick={() => setCounter(counter + 1)}>counter+</button> */}
+      <div>
+      ClockByMakson: {currentDate.getHours()}:{currentDate.getMinutes()}:{currentDate.getSeconds()}
+      </div>
+      
     </>
   );
 };
